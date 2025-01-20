43a5650247738410079b77db4dc841823e18da06
Maxim <karimov.maxdev@gmail.com>
2025-01-20 17:22:36 +0400

    upd config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://github.com/karimovmaxdev/drag-and-drop"
-})
+  base: "/drag-and-drop",
+});
